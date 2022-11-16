64792b562d2b9ebe665f0272a8dbc1e5afea4995
starydv7 <pawanstar72926@gmail.com>
2022-11-16 12:29:39 +0530

    styling and logic

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import "./style.css";
 
+function reducer(state, action) {
+    
+}
 const Main = () => {
+    const[{currentOperand,previousOperand,operation},dispatch]=useReducer(reducer,[])
 
   return (
     <div className="cal-grid">
       <div className="output">
-        <div className="pre-operand"></div>
-        <div className="curr-operand"></div>
+              <div className="pre-operand">{previousOperand}{operation}</div>
+              <div className="curr-operand">{currentOperand}</div>
       </div>
       <button className="span-two">Ac</button>
-      <button>DELETE</button>
+      <button>DEL</button>
       <button>%</button>
       <button>1</button>
       <button>2</button>
